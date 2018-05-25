a566b6ade16e01c00ff708321c611e02770b092c
lantern <bin1813@163.com>
2018-05-25 14:12:22 +0800

    加载图片

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
                 'style-loader',
                 'css-loader'
             ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
         }]
-    }
+    },
+
 };
